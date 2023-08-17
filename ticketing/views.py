f1fd4221afe8e872e96b072e27bf62798c086d4c
HamidReza Sadeghzadeh <hr.m.szh@gmail.com>
2023-08-17 13:33:20 +0330

    Update views.py

--- ticketing/views.py
+++ ticketing/views.py
@@ -1,8 +1,9 @@
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+
 from base.viewsets import ModelViewSet
+
 from .models import *
 from .serializers import *
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
-from django.shortcuts import get_object_or_404
 
 
 class TicketView(ModelViewSet):
@@ -14,20 +15,19 @@ class TicketView(ModelViewSet):
         "partial_update": [IsAdminUser],
         "destroy": [IsAdminUser],
     }
-    filterset_fields = ['created_date', 'modified_date', 'is_archive', 'status', 'department', 'priority']
+    filterset_fields = ['created_date', 'modified_date',
+                        'is_archive', 'status', 'department', 'priority']
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
             return TicketItemSerializer
+        elif self.action == 'list':
+            return TicketSerializer
 
-        return TicketSerializer
+        return CreateTicketSerializer
 
     def get_queryset(self):
-        return Ticket.objects.filter(client=self.request.user)
-
-    def get_object(self):
-        pk = self.kwargs['pk']
-        return get_object_or_404(Ticket, client=self.request.user, pk=pk)
+        return Ticket.objects.filter(client=self.request.user).select_related('client', 'department', 'priority', 'status')
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
