c18a7cf28f12658d4770f2a9cbbf2bf47fcced0e
HamidReza Sadeghzadeh <hr.m.szh@gmail.com>
2023-08-17 13:32:55 +0330

    Update serializers.py

--- ticketing/serializers.py
+++ ticketing/serializers.py
@@ -1,3 +1,4 @@
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
 
 from base.base_serializers import ModelSerializer
@@ -33,10 +34,17 @@ class TicketSerializer(ModelSerializer):
         fields = '__all__'
         read_only_fields = ('is_archive', 'status')
 
-    def create(self, validated_data):
-        user = self.context.get('user')
-        validated_data['client'] = user
-        return super().create(validated_data)
+
+class TicketMessageSerializer(ModelSerializer):
+    files = serializers.SerializerMethodField()
+
+    class Meta:
+        model = TicketMessage
+        exclude = ('ticket',)
+        read_only_fields = ('user',)
+
+    def get_files(self, obj):
+        return MessageFileSerializer(MessageFile.objects.get(message=obj).file, many=True).data
 
 
 class TicketItemSerializer(ModelSerializer):
@@ -51,36 +59,58 @@ class TicketItemSerializer(ModelSerializer):
         read_only_fields = ('is_archive', 'status')
 
     def get_messages(self, obj):
-        return TicketMessageSerializer(TicketMessage.objects.get(ticket=obj).message, many=True).data
+        return TicketMessageSerializer(obj.ticketmessage_set.all(), many=True).data
+
+
+class CreateTicketSerializer(ModelSerializer):
+    message = TicketMessageSerializer(write_only=True)
+
+    class Meta:
+        model = Ticket
+        exclude = ('client',)
+        read_only_fields = ('status',)
+
+    def create(self, validated_data):
+        user = self.context.get('user')
+        validated_data['client'] = user
+        message = validated_data.pop('message')
+        obj = super().create(validated_data)
+        for msg in message:
+            TicketMessage.objects.create(
+                ticket=obj, user=user, content=msg)
+        return obj
 
 
 class MessageFileSerializer(ModelSerializer):
     class Meta:
         model = MessageFile
-        fields = '__all__'
-
-
-class TicketMessageSerializer(ModelSerializer):
-    files = serializers.SerializerMethodField()
-
-    class Meta:
-        model = TicketMessage
-        fields = '__all__'
-
-    def get_files(self, obj):
-        return MessageFileSerializer(MessageFile.objects.get(message=obj).file, many=True).data
+        exclude = ('message',)
 
 
 class CreateTicketMessageSerializer(ModelSerializer):
-    files = MessageFileSerializer(many=True)
+    files = MessageFileSerializer(
+        many=True, required=False, write_only=True)
 
     class Meta:
         model = TicketMessage
-        fields = '__all__'
+        exclude = ('user',)
+
+    def validate(self, attrs):
+        if attrs['ticket'].status.title == 'closed':
+            raise serializers.ValidationError(_('The ticket is closed.'))
+
+        return super().validate(attrs)
 
     def create(self, validated_data):
+        user = self.context.get('user')
+        validated_data['user'] = user
+
+        files = []
+        if validated_data.get('files'):
+            files = validated_data.pop('files')
+
         obj = super().create(validated_data)
-        for file in validated_data['files']:
+        for file in files:
             MessageFile.objects.create(message=obj, file=file)
 
         return obj
